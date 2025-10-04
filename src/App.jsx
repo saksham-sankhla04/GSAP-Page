8037c1cb4d1c9c638975a6829f6cf7def4f2b395
Saksham Sankhla <sakshamsankhla767@gmail.com>
2025-10-04 21:38:32 +0530

    Implemented Navbar and its animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./componenets/navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl text-indigo-300">Hello</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 }
 
