7ae8bc5598a2cf9323f1c03e5f3970c7cbe79fb9
Saksham Sankhla <sakshamsankhla767@gmail.com>
2025-10-05 09:40:10 +0530

    Implemented Hero Section and its animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./componenets/navbar";
+import Hero from "./componenets/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <main>
       <Navbar />
+      <Hero />
     </main>
   );
 }
